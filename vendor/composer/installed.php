03caa8d566be49f000fed4d9ab2d9c38d8f3b94c
ExtBot <flarums-com@gooof.de>
2021-04-07 03:04:00 +0200

    composer update 07.04.2021

---
+++ vendor/composer/installed.php
@@ -0,0 +1,346 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '28469af18f479620b65f9d3ac5e1d9371a64b308',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '28469af18f479620b65f9d3ac5e1d9371a64b308',
+    ),
+    'ahrasis/flarum-ext-malaysian' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '3b25dd7cc5c9909e84f21abac10160d35723db36',
+    ),
+    'bmain/flarum-ext-romanian' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.0.x-dev',
+      ),
+      'reference' => 'fc16c4b0da26f7969b8d4ab66a546e094716b619',
+    ),
+    'cbmainz/flarum-de' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'ee9ed874cf6202d3202a6b559cc5fb4c7028031f',
+    ),
+    'comocozy/flarum-ext-korean' => 
+    array (
+      'pretty_version' => 'v1.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cd164219a77b306f2f00740984637edbd0477d4c',
+    ),
+    'csineneo/flarum-ext-traditional-chinese' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '0.1.x-dev',
+      ),
+      'reference' => '9141d1662efcff292b83bdd0c5f180b8c139fda5',
+    ),
+    'eikoninaru/flarum-ext-ukrainian' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.0.x-dev',
+      ),
+      'reference' => 'e1bbd7759c627baf9ac58b940fa828580e9db210',
+    ),
+    'elite-devs/flarum-ext-arabic' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'ef04c9ea3202c7f31422b3e0204ef151fbdeea7a',
+    ),
+    'enver/flarum-ext-turkish' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '59f8e986611a2e360fb58d8c5d8822d30066b94e',
+    ),
+    'flabrew/flabrew' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '37ad029bdaae1c2d592fd14eb850ce35eabd6bb0',
+    ),
+    'flarum/core' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'flarum/flarum-ext-english' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '0.1.x-dev',
+      ),
+      'reference' => 'e66508927373b9a3ba067cf0250ec41e1df531ac',
+    ),
+    'flarumes/flarum-ext-spanish' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '709ad219a544e821a9796100e8b28963f44377ae',
+    ),
+    'heidar/flarum-ext-swedish' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '0.1.x-dev',
+      ),
+      'reference' => 'c5eb45391836f22307a64405944a0548e900928b',
+    ),
+    'jsthon/flarum-ext-simplified-chinese' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '10a5a354a25306fe653590cc5271598f4f63ba6c',
+    ),
+    'jubayed/flarum-ext-bengali' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'afafd28b2b5bb9141a62177245d269f2473cbe9e',
+    ),
+    'kassius/flarum-brazilian-portuguese' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'c9ebffa1e6e0b78406c401132ac082b05e499c13',
+    ),
+    'kryonoglou/flarum-ext-greek' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '0.2.x-dev',
+      ),
+      'reference' => 'c4f715892a8e06073cc050e560c84748b9719b45',
+    ),
+    'laptrinhphp/vietnamese' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '0f1cba24505678bb74e0e7e512912cc062e22252',
+    ),
+    'lilcode/flarum-ext-estonian' => 
+    array (
+      'pretty_version' => 'v1.6.2.2',
+      'version' => '1.6.2.2',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b8c06e731853103db0e4603876c4293bd296f4e5',
+    ),
+    'lukaszflorczak/flarum-ext-polish' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '0.1.x-dev',
+      ),
+      'reference' => '13f82b559b14f5ce089f8ade4f4c29959be7612b',
+    ),
+    'manaszon/flarum-ext-hungarian' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '4d49673f9df2dff1e1dcc0fa96597b83382a5289',
+    ),
+    'metastore/flarum-ext-russian' => 
+    array (
+      'pretty_version' => '0.1.0-beta.8-1',
+      'version' => '0.1.0.0-beta8-1',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '64c032103cfa367bdd2d0f805cce2fa892fff8b9',
+    ),
+    'milescellar/lang-french' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.10.x-dev',
+      ),
+      'reference' => 'bd0d01dc8fedbb04d8184b9ce9c892e874151a0a',
+    ),
+    'naga0ka/flarum-ext-japanese' => 
+    array (
+      'pretty_version' => 'v1.5',
+      'version' => '1.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '983b1df01756d3a114f82d319bcf4b53332f4274',
+    ),
+    'nearata/lang-italian' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'bd48f8f5cafde5af0a000f891c36b186c682fff7',
+    ),
+    'nit/flarum-ext-albania' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '0.1.x-dev',
+      ),
+      'reference' => 'c5c14f960d8b78855e5e2ba166fbe443adce5881',
+    ),
+    'nlssn/flarum-ext-swedish' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '5c16f719df17dfe8ebf676307aa6d1d463d48341',
+    ),
+    'pladask/flarum-ext-norwegian-bokmal' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '2e1947f539541df3d0664fbfc8b2d7f7bb4b5717',
+    ),
+    'pladask/flarum-ext-norwegian-nynorsk' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '8d17f76037e389ffb5d03dab1121ade36a4dfdc4',
+    ),
+    'realodix/flarum-ext-indonesian' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'ac105fdc4b7c3f6259af0bcf731d8295013d9f26',
+    ),
+    'rodymolenaar/flarum-ext-dutch' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '762ce5a638fd8a32e14a2bf9b1ff0cf7d7a3122b',
+    ),
+    'sijad/flarum-ext-persian' => 
+    array (
+      'pretty_version' => 'v0.0.1',
+      'version' => '0.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '38ebe5dc24be598a653eda1bc14212c51897f310',
+    ),
+    'veriael/flarum-polish' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '71b1dd6447501b43bf8df6842b17d5fba5f940e1',
+    ),
+  ),
+);
